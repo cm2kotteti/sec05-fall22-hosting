45fafc93e18efd173515aae6441625ff4d2ac863
cm2kotteti <89223553+cm2kotteti@users.noreply.github.com>
2022-11-11 22:59:00 -0600

    changed hostname

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http')
 
-const hostname = process.env.hostname || `127.0.0.1`
+const hostname = process.env.hostname
 const port = process.env.port || 3000
 
 const server = http.createServer((req, res) => {
